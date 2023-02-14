1cdc08795d1add26a2a2ef99b02f6e9bfb5d23cc
Udit Srivastava <uditsrivastava6@gmail.com>
2023-02-14 11:31:14 +0530

    fix: change status and emove console logs

--- src/reducer/Table.js
+++ src/reducer/Table.js
@@ -3,17 +3,16 @@ import { ADD_DATA, REMOVE_DATA, CHANGE_STATUS } from "../action/action-types";
 const initialState = [];
 
 const Table = (state = initialState, action) => {
-    console.log(state)
-    switch (action.type) {
-      case ADD_DATA:
-        return [...state, action.payload];
-      case REMOVE_DATA:
-        return state.filter((todo) => todo.id !== action.payload);
-      case CHANGE_STATUS:
-        return state.filter((todo) => todo.id === action.payload);  
-      default:
-        return state;
-}
+  switch (action.type) {
+    case ADD_DATA:
+      return [...state, action.payload];
+    case REMOVE_DATA:
+      return state.filter((todo) => todo.id !== action.payload);
+    case CHANGE_STATUS:
+      return state.map((content, i) => content.id === action.payload ? {...content, status: "completed"} : content)  
+    default:
+      return state;
+  }
 }
 
 export default Table;
